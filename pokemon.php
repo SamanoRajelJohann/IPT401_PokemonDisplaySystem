10f9f7c587837f21b42c76cfdd98518402db1206
Radgelwashere <142951831+SamanoRajelJohann@users.noreply.github.com>
2024-05-26 03:25:47 +0800

    Fixed a bug

--- pokemon.php
+++ pokemon.php
@@ -1,68 +1,178 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Dark Admin by Bootstrapious.com</title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="robots" content="all,follow">
-  <!-- Choices.js-->
-  <link rel="stylesheet" href="vendor/choices.js/public/assets/styles/choices.min.css">
-  <!-- Google fonts - Muli-->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,700">
-  <!-- theme stylesheet-->
-  <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
-  <!-- Custom stylesheet - for your changes-->
-  <link rel="stylesheet" href="css/custom.css">
-  <!-- Favicon-->
-  <link rel="shortcut icon" href="img/favicon.ico">
-  <!-- Tweaks for older IEs-->
-  <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-  <style>
-    .pokemon-card {
-      width: 24rem;
-      margin: 1rem auto;
-      height: 400px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-    }
-    .pokemon-card img {
-      object-fit: cover;
-      width: 100%;
-      height: 300px;
-    }
-  </style>
-</head>
-<body>
-  <header class="header">
-    <!-- Navbar code -->
-  </header>
-  <div class="d-flex align-items-stretch">
-    <!-- Sidebar Navigation-->
-    <nav id="sidebar">
-      <!-- Sidebar code -->
-    </nav>
-    <div class="page-content">
-      <!-- Page Header-->
-      <div class="bg-dash-dark-2 py-4">
-        <div class="container-fluid">
-          <h2 class="h5 mb-0">Pokemon</h2>
+  <head> 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dark Admin by Bootstrapious.com</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Choices.js-->
+    <link rel="stylesheet" href="vendor/choices.js/public/assets/styles/choices.min.css">
+    <!-- Google fonts - Muli-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,700">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+  <body>
+    <header class="header">   
+      <nav class="navbar navbar-expand-lg py-3 bg-dash-dark-2 border-bottom border-dash-dark-1 z-index-10">
+        <div class="search-panel">
+          <div class="search-inner d-flex align-items-center justify-content-center">
+            <div class="close-btn d-flex align-items-center position-absolute top-0 end-0 me-4 mt-2 cursor-pointer"><span>Close </span>
+                  <svg class="svg-icon svg-icon-md svg-icon-heavy text-gray-700 mt-1">
+                    <use xlink:href="#close-1"> </use>
+                  </svg>
+            </div>
+            <div class="row w-100">
+              <div class="col-lg-8 mx-auto">
+                <form class="px-4" id="searchForm" action="#">
+                  <div class="input-group position-relative flex-column flex-lg-row flex-nowrap">
+                    <input class="form-control shadow-0 bg-none px-0 w-100" type="search" name="search" placeholder="What are you searching for...">
+                    <button class="btn btn-link text-gray-600 px-0 text-decoration-none fw-bold cursor-pointer text-center" type="submit">Search</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <!-- Breadcrumb-->
-      <div class="container-fluid py-2">
-        <nav aria-label="breadcrumb">
-          <ol class="breadcrumb mb-0 py-3 px-0">
-            <li class="breadcrumb-item"><a href="home.html">Home</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Pokemon</li>
-          </ol>
-        </nav>
-      </div>
-      <!-- Pokemon cards and randomizer button -->
+        <div class="container-fluid d-flex align-items-center justify-content-between py-1">
+          <div class="navbar-header d-flex align-items-center"><a class="navbar-brand text-uppercase text-reset" href="home.html">
+              <div class="brand-text brand-big"><strong class="text-primary">POKEMON</strong></div>
+              <div class="brand-text brand-sm"><strong class="text-primary">P</strong><strong>O</strong></div></a>
+            <button class="sidebar-toggle">
+                  <svg class="svg-icon svg-icon-sm svg-icon-heavy transform-none">
+                    <use xlink:href="#arrow-left-1"> </use>
+                  </svg>
+            </button>
+          </div>
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item"><a class="search-open nav-link px-0" href="#">
+                    <svg class="svg-icon svg-icon-xs svg-icon-heavy text-gray-700">
+                      <use xlink:href="#find-1"> </use>
+                    </svg></a></li>
+            
+            <!-- Tasks dropdown                   -->
+            <li class="list-inline-item dropdown px-lg-2"><a class="nav-link text-reset px-1 px-lg-0" id="navbarDropdownMenuLink2" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <svg class="svg-icon svg-icon-xs svg-icon-heavy">
+                      <use xlink:href="#paper-stack-1"> </use>
+                    </svg><span class="badge bg-dash-color-3">9</span></a>
+              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink2">
+                <li><a class="dropdown-item" href="#">
+                    <div class="d-flex justify-content-between mb-1"><strong>Task 1</strong><span>40% complete</span></div>
+                    <div class="progress" style="height: 2px">
+                      <div class="progress-bar bg-dash-color-1" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div></a></li>
+                <li><a class="dropdown-item" href="#">
+                    <div class="d-flex justify-content-between mb-1"><strong>Task 2</strong><span>20% complete</span></div>
+                    <div class="progress" style="height: 2px">
+                      <div class="progress-bar bg-dash-color-2" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div></a></li>
+                <li><a class="dropdown-item" href="#">
+                    <div class="d-flex justify-content-between mb-1"><strong>Task 3</strong><span>70% complete</span></div>
+                    <div class="progress" style="height: 2px">
+                      <div class="progress-bar bg-dash-color-3" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div></a></li>
+                <li><a class="dropdown-item" href="#">
+                    <div class="d-flex justify-content-between mb-1"><strong>Task 4</strong><span>40% complete</span></div>
+                    <div class="progress" style="height: 2px">
+                      <div class="progress-bar bg-dash-color-4" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div></a></li>
+                <li><a class="dropdown-item" href="#">
+                    <div class="d-flex justify-content-between mb-1"><strong>Task 5</strong><span>30% complete</span></div>
+                    <div class="progress" style="height: 2px">
+                      <div class="progress-bar bg-dash-color-1" role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div></a></li>
+                <li>           <a class="dropdown-item text-center" href="#"> <strong>See All Tasks <i class="fas fa-angle-right ms-1"></i></strong></a></li>
+              </ul>
+            </li>
+            
+            <!-- Languages dropdown    -->
+            <li class="list-inline-item dropdown"><a class="nav-link dropdown-toggle text-sm text-reset px-1 px-lg-0" id="languages" rel="nofollow" data-bs-target="#" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="img/flags/16/GB.png" alt="English"><span class="d-none d-sm-inline-block ms-2">English</span></a>
+              <ul class="dropdown-menu dropdown-menu-end mt-sm-3 dropdown-menu-dark" aria-labelledby="languages">
+                <li><a class="dropdown-item" rel="nofollow" href="#"> <img class="me-2" src="img/flags/16/DE.png" alt="English"><span>German</span></a></li>
+                <li><a class="dropdown-item" rel="nofollow" href="#"> <img class="me-2" src="img/flags/16/FR.png" alt="English"><span>French                   </span></a></li>
+              </ul>
+            </li>
+            <li class="list-inline-item logout px-lg-2">                 <a class="nav-link text-sm text-reset px-1 px-lg-0" id="logout" href="login.html"> <span class="d-none d-sm-inline-block">Logout </span>
+                    <svg class="svg-icon svg-icon-xs svg-icon-heavy">
+                      <use xlink:href="#disable-1"> </use>
+                    </svg></a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <div class="d-flex align-items-stretch">
+      <!-- Sidebar Navigation-->
+      <nav id="sidebar">
+        <!-- Sidebar Header-->
+        <div class="sidebar-header d-flex align-items-center p-4">
+    
+        </div><span class="text-uppercase text-gray-600 text-xs mx-3 px-2 heading mb-2">Main</span>
+        <ul class="list-unstyled">
+              <li class="sidebar-item"><a class="sidebar-link" href="home.html"> 
+                      <svg class="svg-icon svg-icon-sm svg-icon-heavy">
+                        <use xlink:href="#real-estate-1"> </use>
+                      </svg><span>Home </span></a></li>
+
+              <li class="sidebar-item active"><a class="sidebar-link" href="Pokemon.html"> 
+                <svg fill="currentColor" class="svg-icon svg-icon-sm bi bi-backpack2-fill text-gray-600 " viewBox="0 0 16 16">
+                  <path d="M5 13h6v-3h-1v.5a.5.5 0 0 1-1 0V10H5z"/>
+                  <path d="M6 2v.341C3.67 3.165 2 5.388 2 8v1.191l-1.17.585A1.5 1.5 0 0 0 0 11.118V13.5A1.5 1.5 0 0 0 1.5 15h1c.456.607 1.182 1 2 1h7c.818 0 1.544-.393 2-1h1a1.5 1.5 0 0 0 1.5-1.5v-2.382a1.5 1.5 0 0 0-.83-1.342L14 9.191V8a6 6 0 0 0-4-5.659V2a2 2 0 1 0-4 0m2-1a1 1 0 0 1 1 1v.083a6 6 0 0 0-2 0V2a1 1 0 0 1 1-1m0 3a4 4 0 0 1 3.96 3.43.5.5 0 1 1-.99.14 3 3 0 0 0-5.94 0 .5.5 0 1 1-.99-.14A4 4 0 0 1 8 4M4.5 9h7a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5"/>
+                </svg>
+                        <span>Pokemon</span></a></li>
+
+              <li class="sidebar-item active"><a class="sidebar-link" href="pokedex.html"> 
+                      <svg class="svg-icon svg-icon-sm svg-icon-heavy">
+                        <use xlink:href="#sales-up-1"> </use>
+                      </svg><span>Pokedex</span></a></li>
+           
+              <li class="sidebar-item"><a class="sidebar-link" href="#exampledropdownDropdown" data-bs-toggle="collapse"> 
+                <svg fill="currentColor" class="svg-icon svg-icon-sm bi bi-map text-gray-600 text-gray-600" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15.817.113A.5.5 0 0 1 16 .5v14a.5.5 0 0 1-.402.49l-5 1a.5.5 0 0 1-.196 0L5.5 15.01l-4.902.98A.5.5 0 0 1 0 15.5v-14a.5.5 0 0 1 .402-.49l5-1a.5.5 0 0 1 .196 0L10.5.99l4.902-.98a.5.5 0 0 1 .415.103M10 1.91l-4-.8v12.98l4 .8zm1 12.98 4-.8V1.11l-4 .8zm-6-.8V1.11l-4 .8v12.98z"/>
+                </svg>
+                      <span>Regions</span></a>
+                <ul class="collapse list-unstyled " id="exampledropdownDropdown">
+                  <li><a class="sidebar-link" href="#">Kanto</a></li>
+                  <li><a class="sidebar-link" href="#">Johto</a></li>
+                  <li><a class="sidebar-link" href="#">Hoenn</a></li>
+                  <li><a class="sidebar-link" href="#">Sinnoh</a></li>
+                  <li><a class="sidebar-link" href="#">Unova</a></li>
+                  <li><a class="sidebar-link" href="#">Kalos</a></li>
+                </ul>
+              </li>
+              <li class="sidebar-item"><a class="sidebar-link" href="login.html"> 
+                      <svg class="svg-icon svg-icon-sm svg-icon-heavy">
+                        <use xlink:href="#disable-1"> </use>
+                      </svg><span>Login page </span></a></li>
+      </nav>
+
+      <div class="page-content">
+            <!-- Page Header-->
+            <div class="bg-dash-dark-2 py-4">
+              <div class="container-fluid">
+                <h2 class="h5 mb-0">Pokemon</h2>
+              </div>
+            </div>
+
+            <!-- Breadcrumb-->
+            <div class="container-fluid py-2">
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0 py-3 px-0">
+                  <li class="breadcrumb-item"><a href="home.html">Home</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Pokemon</li>
+                </ol>
+              </nav>
+            </div>
+            
+            <!-- Pokemon cards and randomizer button -->
       <div id="pokemon-container">
         <?php
         // Include the PHP script six times to display six random Pokémon
@@ -74,23 +184,51 @@
       <div class="text-center">
         <button id="randomizeButton" class="btn btn-primary" onclick="location.reload();">Randomize Pokémon</button>
       </div>
-      <!-- Page Footer-->
-      <footer class="position-absolute bottom-0 bg-dash-dark-2 text-white text-center py-3 w-100 text-xs" id="footer">
-        <div class="container-fluid text-center">
-          <p class="mb-0 text-dash-gray">2021 &copy; Your company. Design by <a href="https://bootstrapious.com">Bootstrapious</a>.</p>
-        </div>
-      </footer>
+
+        <!-- Page Footer-->
+        <footer class="position-absolute bottom-0 bg-dash-dark-2 text-white text-center py-3 w-100 text-xs" id="footer">
+          <div class="container-fluid text-center">
+            <!-- Please do not remove the backlink to us unless you support us at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
+            <p class="mb-0 text-dash-gray">2021 &copy; Your company. Design by <a href="https://bootstrapious.com">Bootstrapious</a>.</p>
+          </div>
+        </footer>
+      </div>
     </div>
-  </div>
-  <!-- JavaScript files-->
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="vendor/just-validate/js/just-validate.min.js"></script>
-  <script src="vendor/chart.js/Chart.min.js"></script>
-  <script src="vendor/choices.js/public/assets/scripts/choices.min.js"></script>
-  <script src="js/charts-custom.js"></script>
-  <!-- Main File-->
-  <script src="js/front.js"></script>
-  <!-- FontAwesome CSS - loading as last, so it doesn't block rendering-->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-</body>
+    <!-- JavaScript files-->
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/just-validate/js/just-validate.min.js"></script>
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="vendor/choices.js/public/assets/scripts/choices.min.js"></script>
+    <script src="js/charts-custom.js"></script>
+    <!-- Main File-->
+    <script src="js/front.js"></script>
+    <script>
+      // ------------------------------------------------------- //
+      //   Inject SVG Sprite - 
+      //   see more here 
+      //   https://css-tricks.com/ajaxing-svg-sprite/
+      // ------------------------------------------------------ //
+      function injectSvgSprite(path) {
+      
+          var ajax = new XMLHttpRequest();
+          ajax.open("GET", path, true);
+          ajax.send();
+          ajax.onload = function(e) {
+          var div = document.createElement("div");
+          div.className = 'd-none';
+          div.innerHTML = ajax.responseText;
+          document.body.insertBefore(div, document.body.childNodes[0]);
+          }
+      }
+      // this is set to BootstrapTemple website as you cannot 
+      // inject local SVG sprite (using only 'icons/orion-svg-sprite.svg' path)
+      // while using file:// protocol
+      // pls don't forget to change to your domain :)
+      injectSvgSprite('https://bootstraptemple.com/files/icons/orion-svg-sprite.svg'); 
+      
+      
+    </script>
+    <!-- FontAwesome CSS - loading as last, so it doesn't block rendering-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  </body>
 </html>
